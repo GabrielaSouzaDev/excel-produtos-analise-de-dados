3f1875d6e3cea9d9cbb119447aeb02eb97e8cd10
Gabriela <mr27612@gmail.com>
2026-05-13 21:18:17 -0300

    analise da planilha

--- excel.py
+++ excel.py
@@ -1,3 +1,11 @@
 # Autor: Gabriela Souza
 # Projeto de analise de dados no excel com Pandas
 
+import pandas as pd
+
+planilha = pd.read_excel('vendas.xlsx')
+
+resultado = planilha.groupby(['ID_Produto','Nome_Produto'])['Quantidade_Vendida'].sum()
+
+for (id, produto), quantidade in resultado.items():
+    print(f'{id} - {produto} - {quantidade}')
